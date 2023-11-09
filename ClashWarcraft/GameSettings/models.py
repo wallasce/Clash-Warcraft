74065b11f0cf7cb94421b1988add165ea87ed988
wallasce <lwallasce@outlook.com>
2023-11-09 16:48:12 -0300

    Feat: Create GameSettings Model

--- ClashWarcraft/GameSettings/models.py
+++ ClashWarcraft/GameSettings/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class GameSettings(models.Model):
+    passHomeScreen = models.BooleanField()
+    gameMode = models.CharField(max_length=3)
+    faction = models.CharField(max_length=8)
