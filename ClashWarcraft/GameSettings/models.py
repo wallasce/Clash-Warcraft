222b71a2bd7d936b577bc423589f24a8d4f1f9bd
wallasce <lwallasce@outlook.com>
2023-11-09 17:01:07 -0300

    Fix: setGameMode fields should be blank

--- ClashWarcraft/GameSettings/models.py
+++ ClashWarcraft/GameSettings/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class GameSettings(models.Model):
-    passHomeScreen = models.BooleanField()
-    gameMode = models.CharField(max_length=3)
-    faction = models.CharField(max_length=8)
+    passHomeScreen = models.BooleanField(blank=True, default=False)
+    gameMode = models.CharField(max_length=3, blank=True)
+    faction = models.CharField(max_length=8, blank=True)
