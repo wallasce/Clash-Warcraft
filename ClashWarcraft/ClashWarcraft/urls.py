a3accf48e957e17434f2adf61d8c73d9f6f008ed
wallasce <lwallasce@outlook.com>
2023-11-09 15:59:02 -0300

    Feat: Create Mode Select Screen

--- ClashWarcraft/ClashWarcraft/urls.py
+++ ClashWarcraft/ClashWarcraft/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Game.views import game
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("game/", game),
 ]
--- ClashWarcraft/Game/views.py
+++ ClashWarcraft/Game/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def game(request):
+    pathImageModeSelect = 'Game/Image/ModeSelect/'
+
+    parameters = {
+        'types' : [
+            {
+                'value' : 'pvp',
+                'path' : pathImageModeSelect + 'pvp.png',
+            },{
+                'value' : 'pve',
+                'path' : pathImageModeSelect + 'pve.png',
+            },
+        ]        
+    }
+    return render(request, 'Game/modeSelect.html', parameters)
