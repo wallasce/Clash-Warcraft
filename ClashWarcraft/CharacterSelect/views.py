c3914cd7a4cd498935089a6b761155230d3a2621
wallasce <lwallasce@outlook.com>
2023-11-11 18:32:57 -0300

    Feat: Create Reset Team

--- ClashWarcraft/CharacterSelect/views.py
+++ ClashWarcraft/CharacterSelect/views.py
@@ -3,6 +3,15 @@ from Character.models import Character
 from .models import characterSelect
 from .setters import *
 
+def resetTeams() -> None:
+    teams = characterSelect.objects.all()
+    for team in teams:
+        team.tank = ''
+        team.melee = ''
+        team.ranged = ''
+        team.heal = ''
+        team.save()
+
 def selectCharacter(request, factionRequired, typeRequired):
     backgroundPath = 'CharacterSelect/Image/' + factionRequired + 'Background.jpg'
     characterOptions = Character.objects.all().filter(faction = factionRequired , type = typeRequired)
--- ClashWarcraft/Website/views.py
+++ ClashWarcraft/Website/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from CharacterSelect.views import resetTeams
 from GameSettings.views import resetSettings
 
 # Create your views here.
 def homePage(request):
     resetSettings()
+    resetTeams()
     return render(request, 'WebSite/homePage.html')
