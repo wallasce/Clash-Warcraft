29b6a444b58a6808339a9b97c982409bde9d5d2b
wallasce <lwallasce@outlook.com>
2023-11-11 10:54:17 -0300

    Feat: Create CharacterSelect Model

--- ClashWarcraft/CharacterSelect/models.py
+++ ClashWarcraft/CharacterSelect/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class characterSelect(models.Model):
+    tank = models.CharField(max_length=50, blank=True)
+    melee = models.CharField(max_length=50, blank=True)
+    ranged = models.CharField(max_length=50, blank=True)
+    heal = models.CharField(max_length=50, blank=True)
+
+    def __str__(self) -> str:
+        return "Character Selected"
