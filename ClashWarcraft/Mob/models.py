a4043ee822eaff951fc04ba7f83cd13ecda84b69
wallasce <lwallasce@outlook.com>
2023-11-12 10:56:49 -0300

    Feat: Create Model Mob

--- ClashWarcraft/Mob/models.py
+++ ClashWarcraft/Mob/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Mob(models.Model):
+    name = models.CharField(max_length=50)
+    raid = models.CharField(max_length=8)
+    level = models.CharField(max_length=15)
+    attributes = models.ForeignKey("Attributes", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Attributes(models.Model):
+    attributesOf = models.CharField(max_length=50, default="")
+    armor = models.FloatField()
+    power = models.FloatField()
+    stamina = models.FloatField()
+
+    def __str__(self) -> str:
+        return "Attributes of " + self.attributesOf
