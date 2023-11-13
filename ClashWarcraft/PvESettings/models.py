8cef9cf17eb7befe4a5b3ac27d53e0f8e48285ac
wallasce <lwallasce@outlook.com>
2023-11-13 00:11:00 -0300

    Feat: Create PvESettings Model

--- ClashWarcraft/PvESettings/models.py
+++ ClashWarcraft/PvESettings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class PvESetting(models.Model):
+    raid = models.IntegerField(blank=True, default=0)
+    easyMob = models.CharField(max_length=55, blank=True)
+    mediumMob = models.CharField(max_length=55, blank=True)
+    hardMob = models.CharField(max_length=55, blank=True)
+    bossMob = models.CharField(max_length=55, blank=True)
