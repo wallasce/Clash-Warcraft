2814931bcad23b16d6bdfa827584eab7e32bd839
wallasce <lwallasce@outlook.com>
2023-11-13 15:00:29 -0300

    Feat: Create Screen to Play Game

--- ClashWarcraft/Game/views.py
+++ ClashWarcraft/Game/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
-from CharacterSelect.models import characterSelect
-from CharacterSelect.views import selectCharacter
 from GameSettings.models import GameSettings
 from GameSettings.views import modeSelect, factionSelect, InitialPage
 from PvESettings.models import PvESetting
 from PvESettings.views import setMobs
-
-def userSelectCharacter(request, faction, player):
-    playersCharacter = characterSelect.objects.all()
-
-    if playersCharacter[player].tank == '':
-        return selectCharacter(request, faction, 'Tank', player)
-    elif playersCharacter[player].melee == '':
-        return selectCharacter(request, faction, 'Melee', player)
-    elif playersCharacter[player].ranged == '':
-        return selectCharacter(request, faction, 'Ranged', player)
-    elif playersCharacter[player].heal == '':
-        return selectCharacter(request, faction, 'Heal', player)
-    else:
-        return None
-    
+from .wrapper import setParametersGame, userSelectCharacter
 
 def game(request):
     # Set Game Settings.
@@ -48,9 +32,13 @@ def game(request):
         if (userSelect):
             return userSelect
     elif (settings.gameMode == 'pve'):
-        settings = PvESetting.objects.first()
-        settings.raid += 1
-        settings.save()
+        settingsPvE = PvESetting.objects.first()
+        settingsPvE.raid += 1
+        settingsPvE.save()
         
         setMobs()
+
+    # Render Game Screen.
+    parameters = setParametersGame()
+    return render(request, 'Game/game.html', parameters)
     
---
+++ ClashWarcraft/Game/wrapper.py
@@ -0,0 +1,72 @@
+from CharacterSelect.models import characterSelect
+from CharacterSelect.views import selectCharacter
+from GameSettings.models import GameSettings
+from PvESettings.models import PvESetting
+
+# Return a screen to select player characters
+def userSelectCharacter(request, faction, player):
+    playersCharacter = characterSelect.objects.all()
+
+    if playersCharacter[player].tank == '':
+        return selectCharacter(request, faction, 'Tank', player)
+    elif playersCharacter[player].melee == '':
+        return selectCharacter(request, faction, 'Melee', player)
+    elif playersCharacter[player].ranged == '':
+        return selectCharacter(request, faction, 'Ranged', player)
+    elif playersCharacter[player].heal == '':
+        return selectCharacter(request, faction, 'Heal', player)
+    else:
+        return None
+    
+
+
+# Set parameters from Game Screen.
+def setParametersGame() -> dict:
+    parameters = {}
+    parameters['leftSide'] = getPlayerParameters(0)
+
+    settings = GameSettings.objects.first()
+    if (settings.gameMode == 'pve'):
+        settingsPvE = PvESetting.objects.first()
+        parameters['rightSide'] = getMobParameters(settingsPvE)
+        parameters['backgroundPath'] = 'Game/Image/Background/pveBackground'+str(settingsPvE.raid)+'.jpg'
+    elif (settings.gameMode == 'pvp'):
+        parameters['rightSide'] = getPlayerParameters(1)
+        parameters['backgroundPath'] = 'Game/Image/Background/pvpBackground.jpg'
+
+    return parameters
+
+# Format name to remove blank spaces and invalid characters.
+def formatName(name : str) -> str:
+    nameFormated = name.replace("'", '')
+    nameFormated = nameFormated.replace(' ', '')
+    return nameFormated
+
+# Return a list with path for images from characters choosen by player.
+def getPlayerParameters(player : int) -> list:
+    path = 'Game/Image/Cards/'
+    characters = characterSelect.objects.all()[player]
+
+    frontCharacters  = [
+        path + formatName(characters.tank) + '.png',
+        path + formatName(characters.melee) + '.png',
+    ]
+    behindCharacters = [
+        path + formatName(characters.ranged) + '.png',
+        path + formatName(characters.heal) + '.png',
+    ]
+
+    pathsPlayer = behindCharacters + frontCharacters if player == 0 else frontCharacters + behindCharacters 
+    return pathsPlayer
+
+# Return a list with path for images from pve mode.
+def getMobParameters(settingsPvE : PvESetting) -> list:
+    path = 'Game/Image/Cards/'
+    pathsMob = [
+        path + formatName(settingsPvE.easyMob) + '.png',
+        path + formatName(settingsPvE.mediumMob) + '.png',
+        path + formatName(settingsPvE.hardMob) + '.png',
+        path + formatName(settingsPvE.bossMob) + '.png',
+    ]
+
+    return pathsMob
