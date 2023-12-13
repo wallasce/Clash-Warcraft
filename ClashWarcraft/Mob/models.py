e77c739ba569024793afd892bcc6547f03c669df
wallasce <lwallasce@outlook.com>
2023-12-13 14:51:32 -0300

    Feat: Add skills to mob

--- ClashWarcraft/Mob/models.py
+++ ClashWarcraft/Mob/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from Skill.models import Skill
 
 class Mob(models.Model):
     name = models.CharField(max_length=50)
     raid = models.CharField(max_length=8)
     level = models.CharField(max_length=15)
     attributes = models.ForeignKey("Attribute", on_delete=models.CASCADE)
+    skill = models.ManyToManyField(Skill)
 
     def __str__(self) -> str:
         return self.name
