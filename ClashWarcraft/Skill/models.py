b0c243edafaafc5b7ea493a04b8ec4e8e59e6203
wallasce <lwallasce@outlook.com>
2024-01-17 13:50:10 -0300

    Feat: Add name and description in Skill

--- ClashWarcraft/Skill/models.py
+++ ClashWarcraft/Skill/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Skill(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    description = models.CharField(max_length=255, blank=True)
     skillOff = models.CharField(max_length=50, blank=True)
     level = models.IntegerField(blank=True)
     type = models.CharField(max_length=25, blank=True)
--- ClashWarcraft/Website/wrapper.py
+++ ClashWarcraft/Website/wrapper.py
@@ -1,3 +1,5 @@
+from django.db.models.manager import BaseManager
+
 from Character.models import Character
 
 def formatName(name : str):
@@ -18,12 +20,27 @@ def getCharacterParameters(faction : str):
 
     return characterParameters
 
+def getSkillDetails(character : BaseManager[Character]):
+    skillsDetails = []
+    skills = character.skill.all()
+    for skill in skills:
+        skillsDetails.append({
+            'name' : 'skill name',
+            'type' : skill.type,
+            'cooldown' : skill.cooldown,
+            'baseEffect' : skill.baseEffect,
+            'imagePath' : '/Skill/Image/'+ str(skill).replace(' ', '') + '.png',
+        })
+
+    return skillsDetails
+
 def getCharacterDetails(name : str):
     character = Character.objects.all().filter(name = name)[0]
     details = {
         'name' : name,
         'role' : character.type,
         'class' : character.kind,
+        'skills' : getSkillDetails(character),
         'backgroundPath' : 'Website/Image/BackgroundClass/' + formatName(character.kind) + '.avif',
         'charImagePath' : 'Website/Image/CharactersPNG/' + formatName(character.name) + '.png',
     }
